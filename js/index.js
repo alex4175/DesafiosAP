2124df7acca79701e714de45b9168f665c3bacf4
Alex <alexcochi774@gmail.com>
2024-04-15 20:46:10 -0300

    primer desafio

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let producto = "motherboard"
+let precioU = 119
+let cantDeseada = prompt("Digame cuantos productos desea llevarse: ")
+
+let total = precioU * cantDeseada
+
+alert("En total va a gastar " + total)

ef2d2aa1467fbe02f1febba6496e0d4e43955548
Alex <alexcochi774@gmail.com>
2024-05-06 18:47:32 -0300

    actualizar desafio 4

--- Desafio 4/index.js
+++ Desafio 4/index.js
@@ -3,6 +3,6 @@ function sumarProductos (a, b) {
 }
 
 let precioUnitario = 70
-let cantidadDeseada = Number(prompt("Ingrese la cantidad deseada a comprar"))
+let cantidadDeseada = Number(prompt("Ingrese la cantidad deseada a comprar, el precio es de " + precioUnitario))
 
 alert(sumarProductos(precioUnitario, cantidadDeseada))

e5317948b31ab5731999407375e12e7ec952e6d9
Alex <alexcochi774@gmail.com>
2024-05-24 19:53:48 -0300

    Desafio 7

---
+++ Desafio 7/app.js
@@ -0,0 +1,5 @@
+const precioProducto = document.getElementById("precioProducto");
+precioProducto.innerText = "$700";
+
+const tituloProducto = document.querySelector("#producto");
+tituloProducto.style.color = "cyan";
